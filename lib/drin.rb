9a3ab87ce23301fdbf2bbe13e2adc673d5b04366
jiikko <n905i.1214@gmail.com>
2018-05-11 00:16:59 +0900

    wip

--- lib/drin.rb
+++ lib/drin.rb
@@ -14,3 +14,22 @@ def Object.const_missing(name)
     super
   end
 end
+
+
+__END__
+'json', run endrell loop
+
+class Object
+  def method_missing(name, *args)
+    begin
+      require name.to_s.downcase
+      return self.send(name)
+    rescue LoadError
+      super
+    end
+  end
+
+  def to_ary
+    nil
+  end
+end
